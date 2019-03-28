6f200523bb964d52152d02014add94882e693001
kienth <kienth95@gmail.com>
2019-03-28 18:22:45 +0700

    yeah

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,20 +1,33 @@
 <template>
     <div>
-        <div>
-            <b-carousel
-                    id="carousel-fade"
-                    style="text-shadow: 0px 0px 2px #000"
-                    :interval="4000"
-                    img-width="1024"
-                    img-height="480"
-                    controls
-                    fade
-                    indicators
-            >
-                <b-carousel-slide id="slide-1" caption="" :img-src="require('../../assets/images/team1.jpg')"/>
-            </b-carousel>
-        </div>
+
         <div class="screen-home">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="../../assets/images/nhau-dem-home.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../../assets/images/vidic.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../../assets/images/vidic.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
             <div class="row div-match">
                 <div class="col-lg-4 last-match">
                     <h4>TRẬN TRƯỚC</h4>
